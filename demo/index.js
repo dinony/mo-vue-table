324e62a5b1a4d74549aae12776c35796c91061b8
dinony <contact@dinony.com>
2017-08-02 14:25:36 +0200

    Add json loader plugin

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import players from './players.json'
 
 const app = new Vue({
   el: '#app',
--- demo/systemjs.config.js
+++ demo/systemjs.config.js
@@ -3,10 +3,12 @@ SystemJS.config({
     'npm:': 'https://unpkg.com/'
   },
   map: {
+    'plugin-json': 'npm:systemjs-plugin-json/json.js',
     'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
     'systemjs-babel-build': 'npm:systemjs-plugin-babel/systemjs-babel-browser.js',
 
     'vue': 'npm:vue/dist/vue.min.js'
   },
+  meta: { '*.json': { loader: 'plugin-json' } },
   transpiler: 'plugin-babel'
 })
