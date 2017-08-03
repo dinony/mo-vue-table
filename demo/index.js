c0d48ece7de8d911f561dc8cc55cf88846250b33
dinony <contact@dinony.com>
2017-08-03 15:31:25 +0200

    Use plugin. Prepare table template

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import {MoVueTable} from 'mo-vue-table'
+
 import players from './players.json'
 
 import PlayersTable from './PlayersTable/PlayersTable.js'
+
+Vue.use(MoVueTable)
 
 Vue.component('players-table', PlayersTable)
 
