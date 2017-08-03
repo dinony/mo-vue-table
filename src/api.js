a4867f64f1dc300c47c90f98500958c42d9cdce6
dinony <contact@dinony.com>
2017-08-03 15:31:09 +0200

    Expose directive via plugin

--- src/api.js
+++ src/api.js
@@ -1,4 +1,14 @@
 export {moBaseTable} from './baseTable'
 export {moLocalTable} from './localTable'
 export {moRemoteTable} from './remoteTable'
-export {moToggleOrderBy} from './toggleOrderBy'
+
+import {moToggleOrderBy} from './toggleOrderBy'
+
+export {moToggleOrderBy}
+
+export const MoVueTable = {
+  install: Vue => {
+    // Expose directive via plugin
+    Vue.directive('mo-toggle-orderby', moToggleOrderBy)
+  }
+}
