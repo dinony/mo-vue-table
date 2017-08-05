10c34876500444cc3327c850193051c8d487b4db
Onur D <contact@dinony.com>
2017-08-05 20:27:33 +0200

    Cleanup demo

--- demo/PlayersTable/PlayersTable.js
+++ demo/PlayersTable/PlayersTable.js
@@ -9,12 +9,6 @@ export default {
     columns: ['id', 'name', 'age', 'points'],
     select: ['id', 'name', 'age', 'points'],
     selectOrdered: [['id', 1], ['name', 2], ['age', 3], ['points', 4]],
-    filtersDescr: {
-      'id': {type: 'number'},
-      'name': {type: 'text'},
-      'age': {type: 'number'},
-      'points': {type: 'number'}
-    },
     query: `{
      "points": {"$gt":600},
      "$or": [
@@ -23,19 +17,6 @@ export default {
      ]
     }`,
     syntaxErr: null,
-    ops: ['=', '<>', '>', '<', '>=', '<=', 'in', 'not in', 'between', 'like'],
-    opNameMap: {
-      '=': '$eq',
-      '<>': '$ne',
-      '>': '$gt',
-      '<': '$lt',
-      '>=': '$gte',
-      '<=': '$lte',
-      'in': '$in',
-      'not in': '$nin',
-      'between': '$btw',
-      'like': '$lk'
-    },
     limits: [3, 5, 10, 25, 50],
     limit: 5
   }),
@@ -46,11 +27,6 @@ export default {
   computed: {
     selected: function() {
       return this.selectOrdered.filter(so => this.select.find(s => s === so[0]))
-    }
-  },
-  methods: {
-    addFilter: function() {
-      this.filters.push({left: this.moSelectedColumns[0][0], op: this.ops[0], right: null})
     }
   },
   watch: {
@@ -83,12 +59,14 @@ export default {
         </span>
         {{select}}
       </div>
+
       <div class="info show">
         <span>Show</span>
         <select v-model="limit">
           <option v-for="limit in limits" :value="limit">{{limit}}</option>
         </select>
       </div>
+
       <table class="players">
         <thead>
           <th v-mo-toggle-orderby="column[0]" :key="column[1]" v-for="column in moSelectedColumns"
